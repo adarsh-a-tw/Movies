28355b0786568b1bcb8954f04caad689e02a94f5
Adarsh A <adarsh.a@thoughtworks.com>
2022-09-09 11:21:16 +0530

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Adarsh A on 09/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
